13e0b4040cc85fb11c492d214881e67ba7560769
Damilola john <97457114+damilolajohn6@users.noreply.github.com>
2025-03-24 04:12:54 +0100

    firebase init

--- src/components/MobileNav.tsx
+++ src/components/MobileNav.tsx
@@ -5,7 +5,7 @@ import { usePathname } from "next/navigation";
 import { Sheet, SheetContent, SheetTrigger } from "./ui/sheet";
 import { CiMenuFries } from "react-icons/ci";
 import { Button } from "@/components/ui/button";
-// import Image from "next/image";
+import { useState } from "react";
 
 const links = [
   { href: "/", label: "Home" },
@@ -17,12 +17,16 @@ const links = [
 
 const MobileNav = () => {
   const pathname = usePathname();
+  const [open, setOpen] = useState(false);
 
   return (
-    <Sheet>
+    <Sheet open={open} onOpenChange={setOpen}>
       {/* Mobile Menu Trigger */}
-      <SheetTrigger className="flex justify-center items-center">
-        <Button className="bg-[#78706F] hover:bg-[#3f3b3a] p-2 rounded-md">
+      <SheetTrigger asChild>
+        <Button
+          className="bg-[#78706F] hover:bg-[#3f3b3a] p-2 rounded-md"
+          onClick={() => setOpen(true)}
+        >
           <CiMenuFries className="text-[36px] text-white" />
         </Button>
       </SheetTrigger>
@@ -32,14 +36,13 @@ const MobileNav = () => {
         side="right"
         className="flex flex-col bg-[#F5F0E8] p-6 shadow-lg h-full"
       >
-        
-
         {/* Navigation Links */}
         <nav className="flex flex-col text-[#78706F] gap-6 py-16">
           {links.map((link, index) => (
             <Link
               key={index}
               href={link.href}
+              onClick={() => setOpen(false)}
               className={`capitalize font-bold transition-all  ${
                 link.href === pathname
                   ? " border-b-2 pb-1 border-[#DBA56D]"
